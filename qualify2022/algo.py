c8cbffdaf4a88efd82286a2a70a2f3547d933873
H-H2648 <sz2hong@uwaterloo.ca>
2022-02-24 10:41:47 -0800

    algo framework done

--- qualify2022/algo.py
+++ qualify2022/algo.py
@@ -14,13 +14,14 @@ def find_skill(skill, required_level, people_skills, person_is_working, assigned
                         return True
     return False
 
-def find_project(project, people_skills, person_is_working, project_skills):
+def find_project(project, people_skills, person_is_working, project_skills, project_assignment):
     # dictionary: {person: (skill, required_level, current_level_of_person)}
     assigned_people = {}
     for skill in project_skills[project]:
         required_level = project_skills[project][skill]
         if not(find_skill(skill, required_level, people_skills, person_is_working, assigned_people)):
             return False
+    project_assignment[project] = assigned_people
     return True
 
 
@@ -28,13 +29,45 @@ def find_project(project, people_skills, person_is_working, project_skills):
 # people_skills: {person: {skill: level}}
 # person_is_working: {person: is_working/not_is_working}
 # project_info {project_name: (information in the input thingy)}
-# project_is_working {project_name: is_working}
-def solve(people_skills, person_is_working, project_skills, project_is_working):
+# project_is_working {project_name: [is_working, days_till_completion]}
+def solve_day(people_skills, person_is_working, project_skills, project_info, project_is_working, project_assignment):
     for project in project_skills:
-        if project_is_working[project]:
+        if project_is_working[project][0]:
             continue
         else:
-            find_project(project, people_skills, person_is_working, project_skills)
+            if find_project(project, people_skills, person_is_working, project_skills, project_assignment):
+                project_is_working[project] = [True, project_info[0]]
+
+def solve(people_skills, person_is_working, project_info, project_skills):
+    time = 0
+    score = 0
+    project_is_working = {}
+    project_assignment = {}
+    for project in project_info:
+        project_is_working[project] = [False, -1]
+    while len(project_info) > 0:
+        solve_day(people_skills, person_is_working, project_skills, project_is_working, project_assignment)
+
+        for project in project_is_working:
+            if project_is_working[project][0]:
+                project_is_working[project][1] -= 1
+                if project_is_working[project][1] == 0:
+                    score += max(project_info[project][1] - (max(time - project_info[1], 0)), 0)
+                    for person in project_assignment[project]:
+                        people_skills[person][project_assignment[project][person][0]] = project_assignment[project][person][1]
+        time +=1
+    return (score)
+                
+
+    
+    
+
+
+
+
+
+
+
 
 
 
