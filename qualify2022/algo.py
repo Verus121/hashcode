8d95d3f4dc879bc68324ce06ebf57f916ae21b7a
H-H2648 <sz2hong@uwaterloo.ca>
2022-02-24 11:27:44 -0800

    outputs somethign now!

--- qualify2022/algo.py
+++ qualify2022/algo.py
@@ -9,7 +9,7 @@ def find_skill(skill, required_level, people_skills, person_is_working, assigned
                 return True
             if people_skills[person][skill] == required_level - 1:
                 for assigned_person in assigned_people:
-                    if assigned_people[assigned_person] >= required_level:
+                    if people_skills[assigned_person][skill] >= required_level:
                         assigned_people[person] = (skill, required_level, people_skills[person][skill])
                         return True
     return False
@@ -22,6 +22,8 @@ def find_project(project, people_skills, person_is_working, project_skills, proj
         if not(find_skill(skill, required_level, people_skills, person_is_working, assigned_people)):
             return False
     project_assignment[project] = assigned_people
+    for person in project_assignment:
+        person_is_working[person] = True
     return True
 
 
@@ -36,12 +38,14 @@ def solve_day(people_skills, person_is_working, project_skills, project_info, pr
             continue
         else:
             if find_project(project, people_skills, person_is_working, project_skills, project_assignment):
-                project_is_working[project] = [True, project_info[0]]
+                project_is_working[project] = [True, project_info[project][0]]
                 output_string += f"{project}\n"
+                people_string = ""
                 for person in project_assignment[project]:
                     people_string += f"{person} "
                 people_string = people_string[:-1]
                 output_string += f"{people_string}\n"
+    return output_string
 
 def solve_day_sorted_by_time(people_skills, person_is_working, project_skills, project_info, project_is_working, project_assignment, output_string):
     project_list = []
@@ -53,40 +57,42 @@ def solve_day_sorted_by_time(people_skills, person_is_working, project_skills, p
             continue
         else:
             if find_project(project, people_skills, person_is_working, project_skills, project_assignment):
-                project_is_working[project] = [True, project_info[0]]
+                project_is_working[project] = [True, project_info[project][0]]
 
-def solve(people_skills, person_is_working, project_info, project_skills):
+def solve(people_skills, project_info, project_skills):
     time = 0
     score = 0
     project_is_working = {}
     project_assignment = {}
+    person_is_working = {}
+    for person in people_skills:
+        person_is_working[person] = False
+    done_project = set()
     assigned_projects = 0
     output_string = ""
     for project in project_info:
         project_is_working[project] = [False, -1]
-    while len(project_info) > 0:
-        solve_day(people_skills, person_is_working, project_skills, project_is_working, project_assignment, output_string)
+    while len(done_project) < len(project_info):
+        output_string = solve_day(people_skills, person_is_working, project_skills, project_info, project_is_working, project_assignment, output_string)
         for project in project_is_working:
+            if project in done_project:
+                continue
             if project_is_working[project][0]:
                 project_is_working[project][1] -= 1
                 if project_is_working[project][1] == 0:
-                    score += max(project_info[project][1] - (max(time - project_info[1], 0)), 0)
+                    score += max(project_info[project][1] - (max(time - project_info[project][1], 0)), 0)
                     for person in project_assignment[project]:
-                        people_skills[person][project_assignment[project][person][0]] = project_assignment[project][person][1]
-                        person_is_working[person] = [False, -1]
-                    del project_skills[project]
-                    del project_skills[project]
-                    del project_is_working[project]
+                        if people_skills[person][project_assignment[project][person][0]] <= project_assignment[project][person][1]:
+                            people_skills[person][project_assignment[project][person][0]] +=1
+                        person_is_working[person] = False
+                    done_project.add(project)
                     del project_assignment[project]
                     assigned_projects+=1
         time +=1
-    print(f"score: {score}")
+    print(f"score:{score}")
     output_string = f"{assigned_projects}\n{output_string[:-1]}"
+    print(f"output:\n{output_string}")
     return output_string
-                
-
-    
-    
 
 
 
--- qualify2022/main.py
+++ qualify2022/main.py
@@ -1,4 +1,5 @@
 import input
+from algo import solve
 
 a_file = "files/a_an_example.in.txt"
 b_file = "files/b_better_start_small.in.txt"
@@ -7,9 +8,13 @@ d_file = "files/d_dense_schedule.in.txt"
 e_file = "files/e_exceptional_skills.in.txt"
 f_file = "files/f_find_great_mentors.in.txt"
 
-file = a_file
+file = b_file
 
 sim = input.SimulationData(file)
 sim.print_info()
+
+output = solve(sim.contributor_to_skills_dict, sim.projects_info_dict, sim.projects_skills_dict)
+
+
 
 
